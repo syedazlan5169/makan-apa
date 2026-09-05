afcb5a947105aefe3e0e56b746d37bbc3f6a0ca0
Syed Azlan Mac <syedazlan5169@gmail.com>
2026-09-05 13:51:54 +0800

    fix: trust production proxy and secure cookies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->trustProxies(at: [
+            '172.30.10.0/24',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
